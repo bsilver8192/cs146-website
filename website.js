db7f169ad0eb29c0ffb3110a3161566cd58bbf37
Daniel Heyman <daniel.heyman@gmail.com>
2014-11-19 21:34:39 -0500

    Created the design + homepage

---
+++ website.js
@@ -0,0 +1,7 @@
+window.onscroll = function() {
+	var doc = document.documentElement;
+	var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+	var possibleScroll = document.body.clientWidth / 2048 * 1360 - 350;
+	var scroll = (top - 50) / 350 * possibleScroll;
+	document.getElementById("scroll").style.backgroundPosition= "0px -" + (top - 50 > 0 ? scroll : 0) + "px";
+};
