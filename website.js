7a992c535a6999b602db36174542fa2d5dda9ed5
Brian <brian@localhost>
2014-11-20 00:48:10 -0500

    formatting fixes

--- website.js
+++ website.js
@@ -1,6 +1,6 @@
 window.onscroll = function() {
 	var doc = document.documentElement;
-	var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+	var top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
 	var possibleScroll = document.body.clientWidth / 2048 * 1360 - 350;
 	var scroll = (top - 50) / 350 * possibleScroll;
 	document.getElementById("scroll").style.backgroundPosition= "0px -" + (top - 50 > 0 ? scroll : 0) + "px";
