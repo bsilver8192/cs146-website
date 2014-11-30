16948714e6702c8a306418611bea200b76d073aa
Daniel Heyman <daniel.heyman@gmail.com>
2014-11-29 19:59:59 -0500

    Form + other minor changes

--- website.js
+++ website.js
@@ -1,7 +1,17 @@
 window.onscroll = function() {
-  var doc = document.documentElement;
-  var top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
-  var possibleScroll = document.body.clientWidth / 2048 * 1360 - 350;
-  var scroll = (top - 50) / 350 * possibleScroll;
-  document.getElementById("scroll").style.backgroundPosition= "0px -" + (top - 50 > 0 ? scroll : 0) + "px";
+  if( document.getElementById("scroll") != null)
+  {
+    var doc = document.documentElement;
+    var top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+    var possibleScroll = document.body.clientWidth / 2048 * 1360 - 350;
+    var scroll = (top - 50) / 350 * possibleScroll;
+    document.getElementById("scroll").style.backgroundPosition= "0px -" + (top - 50 > 0 ? scroll : 0) + "px";
+  }
 };
+
+
+function submitForm()
+{
+  document.getElementById("form1").style.display = "none";
+  document.getElementById("match").style.display = "block";
+}
